58ce029d004553ecb135a9847f07c27ee925e7d3
Jazmín Rayo <jazmin.rayo.caicedo@utelvt.edu.ec>
2025-10-30 12:27:50 -0500

    actualizado por JazminRayo

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
-const express = require('express');
-const app = express();
-
-// Middleware para interpretar JSON
-app.use(express.json());
-
-// Importar las rutas (que crearemos luego)
-const rutasProductos = require('./routes/productos');
-const rutasUsuarios = require('./routes/usuarios');
-
-// Usar las rutas en el servidor
-app.use('/api/productos', rutasProductos);
-app.use('/api/usuarios', rutasUsuarios);
-
-// Ruta principal
-app.get('/', (req, res) => {
-  res.send('Bienvenido al backend con Express 🚀');
-});
-
-// Puerto del servidor
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+import Server from "./server/server.js";
+import colors from 'colors';
+import dotenv from 'dotenv';
+dotenv.config();
+const server = new Server();
+server.listen();
